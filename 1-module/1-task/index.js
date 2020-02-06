3dba8873ae4d9a118345c9e2d82cc4599d5ad027
Виктория Морозова <viktoriamorozova@iMac-Viktoria.local>
2020-02-07 02:45:59 +0300

    1-module-1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,11 @@
  * @param {number} n
  * @returns {number}
  */
-function factorial(n) {
-
+function factorial(f){
+  let n=1;
+  for(i=1;i<=f;i++) n=n*i;
+  return(n);
 }
+x=0;
+y=factorial(x);
+console.log(y);
